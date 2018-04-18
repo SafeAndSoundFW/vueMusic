bd07be26ebee5605404ecda201201a6e2b2fa3f8
huqionglu <425634840@qq.com>
2018-04-18 09:52:08 +0800

    first commit

---
+++ src/components/singer/singer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="singer">歌手页面</div>
+</template>
+
+<script type="text/ecmascript-6">
+import {getSingerList} from 'api/singer'
+import {ERR_OK} from 'api/config'
+  
+  export default {
+    data() {
+      return {
+      singers:[]
+
+      }
+    },
+    created() {
+      this._getSingerList()
+    },
+    methods:{
+      _getSingerList() {
+        getSingerList().then(res => {
+            if (res.code === ERR_OK ) {
+              this.singers = res.data.list
+              console.log(this.singers)
+            }
+        })
+      }
+    }
+  }
+    
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'babel-polyfill'
+import fastclick from 'fastclick'
+import 'common/stylus/index.styl'
+import VueLazyLoad from 'vue-lazyload'
+Vue.use(VueLazyLoad, {
+  loading:require('common/image/default.png')
+})
+fastclick.attach(document.body)
+
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+
+/* new Vue({
+  el:"#app",
+  render:h => h(App)
+}) */
